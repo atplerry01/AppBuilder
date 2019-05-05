ccf14d3574bcb04903ae7cbdc2064ce6e38e6771
Akinsanya Olanrewaju <atplerry@gmail.com>
2019-05-05 01:04:46 +0100

    Drawer Setup

--- src/navigations/AppDrawerNavigator.js
+++ src/navigations/AppDrawerNavigator.js
@@ -1,6 +1,22 @@
-import { createDrawerNavigator } from 'react-navigation';
+import { Body, Container, Content, Header } from 'native-base';
+import React from 'react';
+import { Image } from 'react-native';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation';
 import DashboardStackNavigator from './DashboardStackNavigator';
 import NewsStackNavigator from './NewsStackNavigator';
+
+const CustomDrawerContentComponent = (props) => (
+  <Container>
+    <Header style={{height: 200}}>
+      <Body>
+        <Image style={{ width: 150, height: 150, borderRadius: 75}} source={require('../../assets/icon.png')} />
+      </Body>
+    </Header>
+    <Content>
+      <DrawerItems {...props} />
+    </Content>
+  </Container>
+);
 
 const AppDrawerNavigator = createDrawerNavigator({
     Dashboard: {
@@ -9,6 +25,12 @@ const AppDrawerNavigator = createDrawerNavigator({
     News: {
       screen: NewsStackNavigator
     }
+  },{
+    initialRouteName: 'Dashboard',
+    contentComponent: CustomDrawerContentComponent,
+    drawerOpenRoute: 'DrawerOpen',
+    drawerCloseRoute: 'DrawerClose',
+    drawerToggleRoute: 'DrawerToggle'
   });
   
   export default AppDrawerNavigator
--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 class DashboardScreen extends Component {
+
+    static navigationOptions = {
+      drawerIcon: (
+        <Image style={{ width: 24, height: 24}} source={require('../../assets/icon.png')} />
+      )
+    }
+
     render() {
       return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
